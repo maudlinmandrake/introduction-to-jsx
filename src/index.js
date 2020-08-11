809fc9c87f14a489a30fd18cfaa489207f7b6bbc
Jenny Mikac <65610141+maudlinmandrake@users.noreply.github.com>
2020-08-11 10:18:06 -0400

    Make render() inject multiple HTML elements Generally speaking, ReactDOM.render() will only allow you to inject one HTML element...unless you wrap all other elements inside a DIV inside the method, in which case the <div> will be your one element. A neat trick!

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById("root"));
+//Note: render will only take ONE HTML element, unless you nest everything inside a DIV
+ReactDOM.render(
+  <div>
+    <h1>Hello, world!</h1>
+    <p>How are you today?</p>
+  </div>,
+  document.getElementById("root")
+);
 
 /*
 NOTE: To do the above in vanilla Javascript, it would look like this:
