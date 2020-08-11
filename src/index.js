326c56e3b00f453ba6c65865513008d58f1fbea1
Jenny Mikac <65610141+maudlinmandrake@users.noreply.github.com>
2020-08-11 10:09:11 -0400

    Add note about React vs vanilla JS

--- src/index.js
+++ src/index.js
@@ -2,3 +2,13 @@ const React = require("react");
 const ReactDOM = require("react-dom");
 
 ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById("root"));
+
+/*
+NOTE: To do the above in vanilla Javascript, it would look like this:
+
+var h1 = document.createElement("h1");
+h1.innerHTML = "Hello World!";
+document.getElementById("root").appendChild(h1);
+
+It's a lot of code!
+*/
